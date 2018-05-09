dc8b0927367dcbe8580b0221d9fd780f684813b8
Kriti Rai <kriti.rai@gmail.com>
2018-05-09 06:34:03 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,8 +3,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
+    time = Time.now
+    t = time.strftime(%H)
 
-    if Time.now > 12
+    if Time.now >= 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
